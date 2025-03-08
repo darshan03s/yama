caa770f2e94c1c4c94b671cdfdcf08a7fcac97ba
Darshan S <darshans2727@gmail.com>
2025-03-08 17:20:19 +0530

    added media card

---
+++ src/components/MediaCard.tsx
@@ -0,0 +1,33 @@
+import { Star } from 'lucide-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+interface MediaCardProps {
+    item: any
+    mediaType: string
+}
+
+const MediaCard: React.FC<MediaCardProps> = ({ item, mediaType }) => {
+    return (
+        <div className="media-card bg-amber-200 rounded-md hover:scale-105 transition-all duration-300" key={item.id}>
+            <Link to={`/${mediaType}/${item.id}`} key={item.id} className='relative'>
+                <img
+                    src={`https://image.tmdb.org/t/p/original${item.poster_path || item.backdrop_path}`}
+                    alt={item.title}
+                    height={500}
+                    width={300}
+                    className='rounded-t-md'
+                />
+
+                <span className='absolute top-2 right-2 bg-amber-300 text-xs p-1 rounded flex gap-1 items-center'><Star className='size-3.5' /> {Math.trunc(item.vote_average * 10) / 10}</span>
+            </Link>
+
+            <div className="info p-1">
+                <h2 className='text-center text-lg truncate font-medium'>{item.title}</h2>
+                <div className="text-center text-xs">{item.release_date || item.first_air_date}</div>
+            </div>
+        </div>
+    )
+}
+
+export default MediaCard
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+import MediaCard from '../components/MediaCard';
 
 const Home: React.FC = () => {
+    const path = useLocation().pathname;
+    const [data, setData] = useState([]);
+    const [mediaType, setMediaType] = useState("");
+
+    let url = "https://api.themoviedb.org/3/";
+
+    console.log(data);
+
+    useEffect(() => {
+        if (path === "/") {
+            url += "discover/movie?include_adult=false";
+            setMediaType("movie");
+        }
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+            },
+            next: { revalidate: 60 },
+        };
+        const fetchData = async () => {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error("Failed to fetch movies");
+            }
+            const resJson = await response.json();
+            setData(resJson.results);
+        }
+
+        fetchData();
+    }, [path]);
+
     return (
-        <div className=''>App</div>
+        <div
+            className='max-w-6xl mx-auto'>
+            <h1 className='text-3xl text-amber-500 py-1 mb-2'>Discover</h1>
+            <div className="media-cards sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 py-2">
+                {data.map((item: any) => (
+                    <MediaCard item={item} mediaType={mediaType} />
+                ))}
+            </div>
+        </div>
     )
 }
 
