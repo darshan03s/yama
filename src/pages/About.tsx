a1fa416a789392bb31314d29c4d57abfee2797bd
Darshan S <darshans2727@gmail.com>
2025-03-12 22:37:24 +0530

    modified about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
     return (
         <Wrapper className='space-y-4 p-4'>
             <h1 className='text-3xl py-2'>About</h1>
-            <p>YAMDb is a movie and TV show database website that provides information about movies and TV shows. It is a user-friendly and easy-to-use website that allows users to search for movies and TV shows, view their details</p>
+            <p>YAMDb(Yet another movie database) is a movie and TV show database website that provides information about movies and TV shows. It is a user-friendly and easy-to-use website that allows users to search for movies and TV shows, view their details</p>
         </Wrapper>
     )
 }
